3336fa74662f931b35fc0ee3516bc4ba41fcff6a
javi <3051496+lifeBalance@users.noreply.github.com>
2022-12-12 18:03:11 +0200

    add node and postgres containers; install nodemon, express; update npm

---
+++ server/server.js
@@ -0,0 +1,12 @@
+import express from 'express'
+
+const app = express()
+
+app.get('/', (req, res) => {
+  res.send('Hello world!\n')
+})
+
+//Listen port
+const PORT = 3000
+app.listen(PORT)
+console.log(`Running on port ${PORT}`)
