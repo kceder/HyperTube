61a9391271b14a002a91888f56ba49fafbe8bbf8
javi <3051496+lifeBalance@users.noreply.github.com>
2022-12-14 14:35:00 +0200

    add full-screen height to layout component

--- client/src/components/layout.jsx
+++ client/src/components/layout.jsx
@@ -2,15 +2,13 @@ import Header from './header'
 
 function Layout(props) {
   return (
-    <>
+    <div className='min-h-screen bg-gray-800'>
       <Header />
 
-      <div className='bg-gray-800'>
-        <div className='max-w-6xl mx-auto'>
-          <main>{props.children}</main>
-        </div>
+      <div className='max-w-6xl mx-auto'>
+        <main>{props.children}</main>
       </div>
-    </>
+    </div>
   )
 }
 export default Layout
