104d7f3394710bc0bb9b6efdc6eaeb5c987860c7
javi <3051496+lifeBalance@users.noreply.github.com>
2023-01-11 18:22:43 +0200

    add downloads model

---
+++ server/src/models/downloads.js
@@ -0,0 +1,71 @@
+import pool from "../lib/db.js"
+
+async function findMovie({ quality, imdb_id }) {
+  const query = `SELECT * FROM downloads
+  WHERE imdb_id = $1
+  AND quality = $2`
+  const values = [ quality, imdb_id ]
+  const result = await pool.query(query, values)
+
+  // console.log('Movie model - found movie',result.rows[0]) // testing
+  const movie = result.rows[0]
+  return user ?? null 
+}
+
+async function saveMovie(movie) {
+  const {
+    imdb_id,
+    quality,
+    completed,
+    path,
+    size,
+    last_watched
+  } = movie
+  const query = `INSERT INTO downloads
+  (
+    imdb_id,
+    quality,
+    completed,
+    path,
+    size,
+    last_watched
+  )
+  VALUES ($1, $2, $3, $4, $5, $6) RETURNING *`
+
+  const values = [
+    imdb_id,
+    quality,
+    completed,
+    path,
+    size,
+    last_watched
+  ]
+
+  const result = await pool.query(query, values)
+
+  // console.log('Movie model - created movie',result.rows[0]) // testing
+  const savedMovie = result.rows[0]
+  return savedMovie ?? null 
+}
+
+async function updateMovie({ imdb_id, quality }) {
+  const { imdb_id, quality } = movie
+  const query = `UPDATE downloads
+  SET completed =  true,
+  WHERE imdb_id = $1
+  AND quality = $2
+  RETURNING *`
+
+  const values = [ imdb_id, quality ]
+  const result = await pool.query(query, values)
+
+  // console.log('Movie model - updated movie',result.rows[0]) // testing
+  const updatedMovie = result.rows[0]
+  return updatedMovie ?? null 
+}
+
+export {
+  findMovie,
+  saveMovie,
+  updateMovie
+}
