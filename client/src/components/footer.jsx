255254246c9b383a2688092c2a8365bab1526821
javi <3051496+lifeBalance@users.noreply.github.com>
2023-01-24 21:34:25 +0200

    make footer more trendy

--- client/src/components/footer.jsx
+++ client/src/components/footer.jsx
@@ -1,10 +1,40 @@
+import separator from '../assets/rose.png'
+
 function Footer() {
   return (
-    <footer className='bg-red-600 text-white mt-auto p-4 border-t-2 border-white'>
-      &copy; 2023
-	  <a href="https://github.com/lifeBalance/" className="ml-3 font-bold hover:text-yellow-500">lifeBalance</a>
-	  <a href="https://github.com/amedeomajer/" className="ml-3 font-bold hover:text-yellow-500">amajer</a>
-	  <a href="https://github.com/kceder/" className="ml-3 font-bold hover:text-yellow-500">kceder</a>
+    <footer className='flex justify-between items-center bg-red-600 text-white mt-auto p-4 border-t-2 border-white'>
+      
+      <span className='font-bold text-xl'>&copy; 1997</span>
+      <div>
+        <a
+          href='https://github.com/amedeomajer/'
+          className='ml-3 font-bold hover:text-blue-500'
+          >
+          amajer
+        </a>
+        <img
+          src={separator}
+          alt='2023-separator'
+          className='hidden md:inline w-12'
+          />
+        <a
+          href='https://github.com/kceder/'
+          className='ml-3 font-bold hover:text-blue-500'
+          >
+          kceder
+        </a>
+        <img
+          src={separator}
+          alt='2023-separator'
+          className='hidden md:inline w-12'
+          />
+        <a
+          href='https://github.com/lifeBalance/'
+          className='ml-3 font-bold hover:text-blue-500'
+          >
+          rodrodri
+        </a>
+      </div>
     </footer>
   )
 }
