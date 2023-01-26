e49d53784f1ee3ca5b4b866dddbc12592322ca87
Amedeo <amajer@student.hive.fi>
2023-01-26 16:53:44 +0200

    added validate token in routers

--- server/src/routes/movies.js
+++ server/src/routes/movies.js
@@ -1,19 +1,13 @@
 import express from 'express'
-import {
-  getListMovies,
-  getMovie,
-  watchMovie
-} from '../controllers/movies.js'
+import { getListMovies, getMovie, watchMovie } from '../controllers/movies.js'
 
 import { validateToken } from '../middlewares/validateToken.js' // plug this middleware later ;-)
 
-
 const router = express.Router()
 // GET /api/movies   ==> fetch movies from APIs and send to client.
-router.get('/movies', getListMovies)
+router.get('/movies', validateToken, getListMovies)
 // GET /api/movies/:id   ==> fetch a given movie from API and send to client.
-router.post('/movies/:id', watchMovie)
-router.get('/movies/:id', getMovie)
-
+router.post('/movies/:id', validateToken, watchMovie)
+router.get('/movies/:id', validateToken, getMovie)
 
 export default router
--- server/src/routes/streams.js
+++ server/src/routes/streams.js
@@ -2,10 +2,9 @@ import express from 'express'
 import { validateToken } from '../middlewares/validateToken.js' // plug this middleware later ;-)
 import { getStream } from '../controllers/streams.js'
 
-
 const router = express.Router()
 
 // GET /api/streams/:id   ==> stream a given movie from API and send to client.
-router.get('/streams/:id/:quality/:hash', getStream)
+router.get('/streams/:id/:quality/:hash', validateToken, getStream)
 
 export default router
