06924de4a8faebc47cf96428885ec4bdd15bbc4a
javi <3051496+lifeBalance@users.noreply.github.com>
2023-01-29 12:09:26 +0200

    disable authorization in streams; replace tabs in indentation in movie-card.jsx

--- client/src/components/movie-card.jsx
+++ client/src/components/movie-card.jsx
@@ -13,39 +13,39 @@ function MovieCard(props) {
     description_full: synopsis
   } = props.movie
 
-  console.log('props.movie', props) // check all props you get
+  // console.log('props.movie', props) // check all props you get
   return (
   <div>
-	<div className='p-3 bg-white bg-opacity-5 text-white rounded-md'>
-	  <div className="grid md:grid-cols-5 md:gap-1">
-		<div className='md:col-span-4'>
-		  <p className='text-xl font-bold text-center'>{title}</p>
-		  <hr className='my-4'/>
-		  <div className='flex justify-between'>
-			<p>
-			  <span className='font-bold'>{year}</span>
-			</p>
-			<p className='flex items-center space-x-2'>
-			  <img src={imdbLogo} alt="imdb logo" className='inline w-8 -mt-1'/>
-			  <span className='pr-3 font-bold'>{imdbRating}</span>
-			</p>
-		  </div>
-		  <p className='mt-3'>
-		  {synopsis}
-		  </p>
-		</div>
-		<img
-		  src={coverUrl}
-		  alt={title}
-		  className='md:w-52 mt-3 md:mt-0 p-3'
-		  onError={(e) => {
-			e.target.onerror = null
-			e.target.src=notFound
-		  }}
-		  />
-	  </div>
-		<MovieCast cast={props.movie.cast}/>
-	</div>
+  <div className='p-3 bg-white bg-opacity-5 text-white rounded-md'>
+    <div className="grid md:grid-cols-5 md:gap-1">
+    <div className='md:col-span-4'>
+      <p className='text-xl font-bold text-center'>{title}</p>
+      <hr className='my-4'/>
+      <div className='flex justify-between'>
+      <p>
+        <span className='font-bold'>{year}</span>
+      </p>
+      <p className='flex items-center space-x-2'>
+        <img src={imdbLogo} alt="imdb logo" className='inline w-8 -mt-1'/>
+        <span className='pr-3 font-bold'>{imdbRating}</span>
+      </p>
+      </div>
+      <p className='mt-3'>
+      {synopsis}
+      </p>
+    </div>
+    <img
+      src={coverUrl}
+      alt={title}
+      className='md:w-52 mt-3 md:mt-0 p-3'
+      onError={(e) => {
+      e.target.onerror = null
+      e.target.src=notFound
+      }}
+      />
+    </div>
+    <MovieCast cast={props.movie.cast}/>
+  </div>
   </div>
   )
 }
--- server/src/routes/streams.js
+++ server/src/routes/streams.js
@@ -1,10 +1,11 @@
 import express from 'express'
-import { validateToken } from '../middlewares/validateToken.js' // plug this middleware later ;-)
+// import { validateToken } from '../middlewares/validateToken.js' // plug this middleware later ;-)
 import { getStream } from '../controllers/streams.js'
 
 const router = express.Router()
 
 // GET /api/streams/:id   ==> stream a given movie from API and send to client.
-router.get('/streams/:id/:quality/:hash', validateToken, getStream)
+// DO NOT ENABLE AUTHORIZATION HERE IF YOU DON'T KNOW HOW TO SEND TOKEN!!!!
+router.get('/streams/:id/:quality/:hash', getStream)
 
 export default router
